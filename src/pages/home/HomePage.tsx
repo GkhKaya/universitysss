4da8173e64da2351e461aab882811245793c9c98
gokhankaya <gkhkaya@gokhankayas-MacBook-Pro.local>
2026-04-19 00:05:23 +0300

    Merkezi tasarim tokenlari ve global stiller eklendi

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,13 @@
+import './HomePage.css'
+
+export function HomePage() {
+  return (
+    <main className="home">
+      <h1>universitysss</h1>
+      <p>
+        Üniversite ortamında soru ve cevap paylaşımı için geliştirilen uygulama. İçerik ve arayüz
+        üzerinde çalışmaya devam ediyoruz.
+      </p>
+    </main>
+  )
+}
