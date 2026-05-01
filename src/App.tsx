b846c1d162ebe275109f09f722523f0660d66df9
Sencer <sencercelik57@gmail.com>
2026-05-01 19:13:59 +0300

    feat: add HomePage, AskPage and update App routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { RegisterPage } from './features/register'
 import { HomePage } from './pages/home/HomePage'
+import { AskPage } from './pages/ask/AskPage'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/ask" element={<AskPage />} />
       </Routes>
     </BrowserRouter>
   )
